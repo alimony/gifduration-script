a2df1e690b5b2f8dcaf7b66d394b044e933635e5
Markus Amalthea Magnuson <markus.magnuson@gmail.com>
2015-07-01 21:06:57 +0200

    Single quotes.

--- gifduration.py
+++ gifduration.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # encoding: utf-8
-"""
+'''
 This script takes one or more animated GIF files as input and calculates their
 total durations, returning zero for non-animated GIF files.
 
 Requires Pillow, formerly known as the Python Imaging Library (PIL).
 
 Code by Markus Amalthea Magnuson <markus.magnuson@gmail.com>
-"""
+'''
 
 from __future__ import print_function
 
@@ -32,21 +32,21 @@ def main(argv=None):
         argv = sys.argv
     try:
         try:
-            opts, args = getopt.getopt(argv[1:], "h:v", ["help", "verbose"])
+            opts, args = getopt.getopt(argv[1:], 'h:v', ['help', 'verbose'])
         except getopt.error as err:
             raise Usage(err.msg)
 
         # Option processing.
         verbose = False
         for option, value in opts:
-            if option in ("-v", "--verbose"):
+            if option in ('-v', '--verbose'):
                 verbose = True
-            if option in ("-h", "--help"):
+            if option in ('-h', '--help'):
                 raise Usage(help_message)
 
     except Usage as err:
-        print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
-        print >> sys.stderr, "\t for help use --help"
+        print >> sys.stderr, sys.argv[0].split('/')[-1] + ': ' + str(err.msg)
+        print >> sys.stderr, '\t for help use --help'
         return 2
 
     # Start processing the images.
@@ -54,9 +54,9 @@ def main(argv=None):
         try:
             im = Image.open(path)
         except IOError as err:
-            print >> sys.stderr, "%s:" % os.path.basename(path)
+            print >> sys.stderr, '%s:' % os.path.basename(path)
             print >> sys.stderr, err
-            print >> sys.stderr, "---"
+            print >> sys.stderr, '---'
             continue
 
         durations = []
@@ -68,15 +68,15 @@ def main(argv=None):
                 pass
 
         if not durations:
-            print("Not an animated GIF image")
+            print('Not an animated GIF image')
         else:
             if verbose:
                 for index, duration in enumerate(durations):
-                    print("Frame %d: %d ms (%0.2f seconds)" % (index + 1, duration, duration / 1000.0))
+                    print('Frame %d: %d ms (%0.2f seconds)' % (index + 1, duration, duration / 1000.0))
             total_duration = sum(durations)
-            print("Total duration: %d ms (%0.2f seconds)" % (total_duration, total_duration / 1000.0))
+            print('Total duration: %d ms (%0.2f seconds)' % (total_duration, total_duration / 1000.0))
 
-        print("---")
+        print('---')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(main())
